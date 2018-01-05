e73b9ede16eb1af4bbc255af09dd92d73dc95e99
Rafael Pacheco <ravpacheco@gmail.com>
2018-01-04 23:54:41 -0200

    Create the project

---
+++ src/Take.Blip.CLI/Take.Blip.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Take.Blip.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
