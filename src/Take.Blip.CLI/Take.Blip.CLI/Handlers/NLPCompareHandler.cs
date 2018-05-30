b0ac22132e818aae2dab335283cb4cb17a6ac80b
Arthur Iperoyg Rodrigues Carvalho (Take.net) <arthurc@take.net>
2018-05-30 18:38:21 -0300

    refactor to use nlpmodel

--- src/Take.Blip.CLI/Take.Blip.CLI/Handlers/NLPCompareHandler.cs
+++ src/Take.Blip.CLI/Take.Blip.CLI/Handlers/NLPCompareHandler.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ITGlobal.CommandLine;
+using Take.BlipCLI.Models;
 using Take.BlipCLI.Services;
 using Take.BlipCLI.Services.Interfaces;
+using Takenet.Iris.Messaging.Resources.ArtificialIntelligence;
 
 namespace Take.BlipCLI.Handlers
 {
@@ -18,6 +20,8 @@ namespace Take.BlipCLI.Handlers
         public INamedParameter<string> Authorization2 { get; internal set; }
         public INamedParameter<string> OutputFilePath { get; internal set; }
         public INamedParameter<ComparisonMethod> Method { get; internal set; }
+        public INamedParameter<string> Bot1Path { get; internal set; }
+        public INamedParameter<string> Bot2Path { get; internal set; }
         public ISwitch Verbose { get; internal set; }
         public bool IsVerbose { get => Verbose.IsSet; }
 
@@ -28,28 +32,25 @@ namespace Take.BlipCLI.Handlers
 
         public override async Task<int> RunAsync(string[] args)
         {
-            if (!Authorization1.IsSet || !Authorization2.IsSet)
-                throw new ArgumentNullException("You must provide two bots parameters for this action. Use '--a1' [--first] and '--a2' [--second] parameters");
+            if (!Authorization1.IsSet && !Bot1Path.IsSet)
+                throw new ArgumentNullException("You must provide a first bot parameter for this action. Use '--a1' [--first] (authorizationKey) or '--p1' [--path1] (path to an exported model) parameters");
+
+            if (!Authorization2.IsSet && !Bot2Path.IsSet)
+                throw new ArgumentNullException("You must provide a second bot parameter for this action. Use '--a2' [--second] (authorizationKey) or '--p2' [--path2] (path to an exported model) parameters");
 
             if (!OutputFilePath.IsSet)
                 throw new ArgumentNullException("You must provide output file path parameter for this action. Use -o [--output] parameter.");
 
+            NLPModel bot1Model = Bot1Path.IsSet ? GetBotModelFromPath(Bot1Path.Value) : await GetBotModelFromAPI(Authorization1.Value);
+            NLPModel bot2Model = Bot2Path.IsSet ? GetBotModelFromPath(Bot2Path.Value) : await GetBotModelFromAPI(Authorization2.Value);
 
-            IBlipAIClient bot1BlipAIClient = new BlipHttpClientAsync(Authorization1.Value);
-            IBlipAIClient bot2BlipAIClient = new BlipHttpClientAsync(Authorization2.Value);
 
-            var bot1Entities = await bot1BlipAIClient.GetAllEntities(verbose: IsVerbose);
-            var bot1Intents = await bot1BlipAIClient.GetAllIntents(verbose: IsVerbose);
-
-            var bot2Entities = await bot2BlipAIClient.GetAllEntities(verbose: IsVerbose);
-            var bot2Intents = await bot2BlipAIClient.GetAllIntents(verbose: IsVerbose);
-
-            foreach (var item1 in bot1Intents)
+            foreach (var item1 in bot1Model.Intents)
             {
                 if (item1.Questions == null) continue;
                 foreach (var question1 in item1.Questions)
                 {
-                    foreach (var item2 in bot2Intents)
+                    foreach (var item2 in bot2Model.Intents)
                     {
                         if (item2.Questions == null) continue;
                         foreach (var question2 in item2.Questions)
@@ -65,6 +66,74 @@ namespace Take.BlipCLI.Handlers
                 }
             }
             return 0;
+        }
+
+        private async Task<NLPModel> GetBotModelFromAPI(string authKey)
+        {
+            IBlipAIClient blipAIClient = new BlipHttpClientAsync(authKey);
+
+            var entities = await blipAIClient.GetAllEntities(verbose: IsVerbose);
+            var intents = await blipAIClient.GetAllIntents(verbose: IsVerbose);
+
+            return new NLPModel { BotId = authKey, Entities = entities, Intents = intents };
+        }
+
+        private NLPModel GetBotModelFromPath(string path)
+        {
+            var intentionsMap = new Dictionary<string, List<string>>();
+
+            var intentCsv = new Chilkat.Csv { HasColumnNames = true };
+            var entityCsv = new Chilkat.Csv { HasColumnNames = true };
+            var answerCsv = new Chilkat.Csv { HasColumnNames = true };
+
+            var intentPath = Path.Combine(path, "intentions.csv");
+            var entityPath = Path.Combine(path, "entities.csv");
+            var answerPath = Path.Combine(path, "answers.csv");
+
+            bool loadedIntent = intentCsv.LoadFile(intentPath);
+            bool loadedEntity = entityCsv.LoadFile(entityPath);
+            bool loadedAnswer = answerCsv.LoadFile(answerPath);
+
+            if (!loadedAnswer || !loadedEntity || !loadedIntent)
+            {
+                return null;
+            }
+
+            var model = new NLPModel
+            {
+                BotId = path,
+                Entities = new List<Entity>(),
+                Intents = new List<Intention>()
+            };
+
+            for (int row = 0; row <= intentCsv.NumRows - 1; row++)
+            {
+                var intentionName = intentCsv.GetCell(row, 0);
+                var question = intentCsv.GetCell(row, 1);
+
+                var questionsList = intentionsMap.ContainsKey(intentionName) ? intentionsMap[intentionName] : new List<string>();
+
+                questionsList.Add(question);
+                intentionsMap[intentionName] = questionsList;
+            }
+
+            foreach (var intent in intentionsMap)
+            {
+                model.Intents.Add(
+                    new Intention
+                    {
+                        Name = intent.Key,
+                        Questions = intent.Value.Select(q =>
+                            new Question
+                            {
+                                Text = q
+                            }).ToArray()
+                    });
+            }
+
+
+
+            throw new NotImplementedException();
         }
 
         internal ComparisonMethod CustomMethodParser(string arg)
